309d31f384127fc7035ca8b856d797c72f8893c7
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-01-03 13:45:15 +0500

    role table is created.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_063111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_roles_on_name", unique: true
+  end
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
