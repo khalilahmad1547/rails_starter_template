529ee9f89d9e7d787cf33e4aaf1de8743db584f3
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-01-03 13:45:56 +0500

    user_role assosiactions are added to table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one :user_role, dependent: :destroy
+
+  delegate :role, to: :user_role
 
   validates_presence_of :first_name, :last_name
 
