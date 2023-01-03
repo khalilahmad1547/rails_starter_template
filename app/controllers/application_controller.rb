44aee0d2d252a3edcc3f070fcf8233a1dd6d93ca
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-01-03 14:26:39 +0500

    rescude from pundit error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render json: { message: 'You are not allowed for this action' }, status: :unauthorized
+  end
 end
