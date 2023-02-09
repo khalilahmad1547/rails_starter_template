8a8e98e06ebd204a94d22e8733c5504bb3ec3ff3
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-02-10 01:16:01 +0500

    basic specs for user model are added.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'should be valid when' do
+    it '.first_name is provided' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'should not be valid when' do
+    it '.first_name is empty' do
+      user = build(:user, first_name: '')
+      expect(user).to_not be_valid
+    end
+
+    it '.last_name is empty' do
+      user = build(:user, last_name: '')
+      expect(user).to_not be_valid
+    end
+
+    it '.email is empty' do
+      user = build(:user, email: '')
+      expect(user).to_not be_valid
+    end
+
+    it '.password is empty' do
+      user = build(:user, password: '')
+      expect(user).to_not be_valid
+    end
+  end
+end
