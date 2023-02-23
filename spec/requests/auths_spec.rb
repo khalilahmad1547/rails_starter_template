99ed36e6078ab40b5034e88e2a199ac63e02ac14
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-02-23 20:40:07 +0500

    auth specs are updated.

--- spec/requests/auths_spec.rb
+++ spec/requests/auths_spec.rb
@@ -2,21 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Auths', type: :request do
   describe 'POST /login' do
-    before do
-      user = { user: { email: Faker::Internet.email,
-                       password: Faker::Internet.password } }
-      user_db = { user: { email: Faker::Internet.email,
-                          password: Faker::Internet.password } }
-      post '/signup', params: user_db
-    end
+    let(:user) { create(:user) }
+
     it 'should be unauthorized when user not exists' do
-      post '/login', params: user
+      post '/login'
       expect(response).to have_http_status :unauthorized
     end
 
     it 'should be success when valid email & password given' do
-      post '/login', params: user_db
-      expect(response).to have_http_status :create
+      post '/login', params: { user: { email: user.email, password: user.password } }
+      expect(response).to be_ok
     end
   end
 end
