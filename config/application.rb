d1a34d55f98c782b80d28a211e7b8a86494a49eb
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-03-02 19:38:00 +0000

    sidekiq is configured for ActiveJobs.

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module RailsStarterTemplate
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
 
     # Don't generate system test files.
     config.generators.system_tests = nil
