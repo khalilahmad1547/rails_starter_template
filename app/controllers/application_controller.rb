ad99b1b9b05d957ac18840001b5d8b07e53db3b4
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-05-29 18:52:22 +0000

    devise_token_auth:install is done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   mount ActionCable.server => '/cable'
   mount Sidekiq::Web => '/sidekiq'
 
