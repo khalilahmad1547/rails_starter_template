17d4440f6059279e9ab59b0585dc54845551afe8
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-05-29 18:40:41 +0000

    devise routes are removed.

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   mount Sidekiq::Web => '/sidekiq'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'welcome#index'
