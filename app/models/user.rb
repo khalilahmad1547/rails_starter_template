544460133e23e3749b55aaa567fdac7e5f76460f
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-07-01 16:27:50 +0000

    name and email validation is added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, format: { with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i }
 end
