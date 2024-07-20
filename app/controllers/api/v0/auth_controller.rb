792b4098b99a83bea6bceea823e53acf050916df
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-07-20 19:52:39 +0000

    auth controller is added.

---
+++ app/controllers/api/v0/auth_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Api::V0
+  class AuthController < ApiController
+    def signup
+      Api::V0::Auth::SignupService.call(params.to_unsafe_h) do |result|
+        result.success { |data| success_response(data, status: :created) }
+        result.failure { |errors| unprocessable_entity(errors) }
+      end
+    end
+
+    def signin
+      Api::V0::Auth::SigninService.call(params.to_unsafe_h) do |result|
+        result.success { |data| success_response(data, status: :created) }
+        result.failure { unauthorized_response }
+      end
+    end
+
+    def refresh
+      Api::V0::Auth::RefreshService.call(params.to_unsafe_h) do |result|
+        result.success { |data| success_response(data, status: :created) }
+        result.failure { unauthorized_response }
+      end
+    end
+  end
+end
