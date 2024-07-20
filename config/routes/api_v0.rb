cfe3d87c19058c1979d7c96197667654638ee897
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-07-20 19:43:19 +0000

    signup, signin & refresh routes are added.

--- config/routes/api_v0.rb
+++ config/routes/api_v0.rb
@@ -3,5 +3,11 @@
 namespace :api do
   namespace :v0 do
     get 'api_status', to: 'example#index'
+
+    scope :auth do
+      post :signup, to: 'auth#signup'
+      post :signin, to: 'auth#signin'
+      post :refresh, to: 'auth#refresh'
+    end
   end
 end
