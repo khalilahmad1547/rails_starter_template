33077ad5b919f8eab999ab6a17791b410f7ad9af
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-07-22 18:39:57 +0000

    user factory is updated.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@tester.com" }
     name { Faker::Name.name }
-    password { 'password' }
+    avatar_url { Faker::Internet.url }
+    provider { 'email' }
+    password { Faker::Internet.password }
   end
 end
