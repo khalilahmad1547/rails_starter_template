b61dd0b22abfd2ff3263939d766c60ca747892f4
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-07-25 18:06:04 +0000

    moved to a concern.

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -2,37 +2,19 @@
 
 module Api::V0
   class ApiController < ActionController::API
-    rescue_from StandardError do |exception|
-      case exception.class.name
-      when ActiveRecord::RecordInvalid.name then unprocessable_entity(exception.message)
-      else process_standard_error(exception)
-      end
-    end
+    include ErrorHandler
+
+    before_action :authenticate_user!
 
     private
 
-    def success_response(data, status: :ok)
-      render json: { success: true, data: JSON.parse(data) }, status:
-    end
+    def authenticate_user!
+      current_user, decoded_token = Api::V0::Jwt::Authenticator.call(
+        headers: request.headers
+      )
 
-    def process_standard_error(exception)
-      render json: { success: false, errors: [exception.message] }, status: :internal_server_error
-    end
-
-    def unauthorized_response(reason = 'You are unauthorized to view this resource')
-      render json: { success: false, errors: [reason] }, status: :unauthorized
-    end
-
-    def not_found_response(reason = 'The requested resource does not exist')
-      render json: { success: false, errors: [reason] }, status: :not_found
-    end
-
-    def unprocessable_entity(reason)
-      render json: { success: false, errors: [reason] }, status: :unprocessable_entity
-    end
-
-    def forbidden_response
-      render json: { success: false, errors: ['forbidden'] }, status: :forbidden
+      @current_user = current_user
+      @decoded_token = decoded_token
     end
   end
 end
