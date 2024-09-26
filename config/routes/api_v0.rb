d8eae94c271785bba5994e07e45118b5b5d13ce4
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-09-26 12:07:42 +0000

    configs are updated.

--- config/routes/api_v0.rb
+++ config/routes/api_v0.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+api_only_routes = %i[index show create update destroy]
+
 namespace :api do
   namespace :v0 do
-    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      registrations: 'api/v0/users/registrations'
-    }
     get 'api_status', to: 'example#index'
+
+    scope :auth do
+      post :signup, to: 'auth#signup'
+      post :signin, to: 'auth#signin'
+      post :refresh, to: 'auth#refresh'
+    end
+
+    resources :accounts, only: api_only_routes
+    resources :categories, only: api_only_routes
+    resources :transactions, only: %i[index show destroy]
+    resources :transfer_transactions, only: %i[create update]
+    resources :individual_transactions, only: %i[create update]
   end
 end
