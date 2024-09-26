0b3d4c98a2a406c2476b6901434f04acb3ea4dd2
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-09-26 12:09:12 +0000

    routes are updated.

--- config/routes/api_v0.rb
+++ config/routes/api_v0.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-api_only_routes = %i[index show create update destroy]
-
 namespace :api do
   namespace :v0 do
     get 'api_status', to: 'example#index'
@@ -11,11 +9,5 @@ namespace :api do
       post :signin, to: 'auth#signin'
       post :refresh, to: 'auth#refresh'
     end
-
-    resources :accounts, only: api_only_routes
-    resources :categories, only: api_only_routes
-    resources :transactions, only: %i[index show destroy]
-    resources :transfer_transactions, only: %i[create update]
-    resources :individual_transactions, only: %i[create update]
   end
 end
