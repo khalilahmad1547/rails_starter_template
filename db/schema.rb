6ae1a5fd5fddc0b950fc32b204f4e3109c55847b
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-09-26 12:10:35 +0000

    db is migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_164001) do
+ActiveRecord::Schema[7.2].define(version: 20_240_913_113_208) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "name", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "uid", default: "", null: false
-    t.string "provider", default: "email", null: false
-    t.boolean "allow_password_change", default: true
-    t.string "image", default: "", null: false
-    t.json "tokens"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table 'blacklisted_tokens', force: :cascade do |t|
+    t.string 'jti'
+    t.bigint 'user_id', null: false
+    t.datetime 'exp', default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['jti'], name: 'index_blacklisted_tokens_on_jti', unique: true
+    t.index ['user_id'], name: 'index_blacklisted_tokens_on_user_id'
   end
 
+  create_table 'refresh_tokens', force: :cascade do |t|
+    t.string 'crypted_token'
+    t.bigint 'user_id', null: false
+    t.datetime 'exp', default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['crypted_token'], name: 'index_refresh_tokens_on_crypted_token', unique: true
+    t.index ['user_id'], name: 'index_refresh_tokens_on_user_id'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'name', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'uid'
+    t.string 'avatar_url'
+    t.string 'provider'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
+
+  add_foreign_key 'blacklisted_tokens', 'users'
+  add_foreign_key 'refresh_tokens', 'users'
 end
