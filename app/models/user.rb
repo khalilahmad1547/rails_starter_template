95b9dbf5927b7f33d03708cbcaf3e9e9d8cd6362
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-09-26 11:46:01 +0000

    models are updated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,6 +10,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i }
 
+  has_many :refresh_tokens, dependent: :delete_all
+  has_many :blacklisted_tokens, dependent: :delete_all
+  has_many :accounts, dependent: :delete_all
+  has_many :categories, dependent: :delete_all
+  has_many :user_transactions, dependent: :delete_all
+  has_many :transactions, dependent: :delete_all
+  has_many :user_groups, dependent: :delete_all
+  has_many :groups, through: :user_groups
+
   def self.from_omniauth(auth)
     data = auth.info
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -19,9 +27,5 @@ class User < ApplicationRecord
       user.name = data.name
       user.avatar_url = data.image
     end
-  end
-
-  def confirmed_at
-    Time.now.utc
   end
 end
